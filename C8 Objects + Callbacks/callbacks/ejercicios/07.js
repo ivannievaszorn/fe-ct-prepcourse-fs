2cc2c3d40d7229c70c0907f946b002fd95605cb2
Griso <ivannievaszorn@gmail.com>
2024-06-11 10:34:54 -0300

    C8 completado

--- C8 Objects + Callbacks/callbacks/ejercicios/07.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -1,7 +1,14 @@
 function filter(arrayOfStrings) {
-   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
-   // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+  // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
+  // Luego retorna un nuevo arreglo con estos elementos.
+  // Tu código:
+  let stringsConA = [];
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    if (arrayOfStrings[i][0] === "a") {
+      stringsConA.push(arrayOfStrings[i]);
+    }
+  }
+  return stringsConA;
 }
 
 module.exports = filter;
